1215d6bde43132f7d06138d474701c9626622568
Gary A. Newsome <garyanewsome@gmail.com>
2016-03-12 15:51:26 -0500

    Formatting change

--- src/digitalpictureframe/DigitalPictureFrame.java
+++ src/digitalpictureframe/DigitalPictureFrame.java
@@ -22,9 +22,11 @@ import javafx.util.Duration;
 
 /**
  * Chapter 15 Programming Challenge 9 Digital Picture Frame
+ *
  * @author garyanewsome
  */
 public class DigitalPictureFrame extends Application {
+
   String[] picName = {"blue.png", "green.png", "red.png"};
   int n = picName.length; // magic number that makes the world go round
   int imgNum = 1; // current image number
@@ -36,14 +38,14 @@ public class DigitalPictureFrame extends Application {
     // create a pane
     VBox pane = new VBox();
     pane.setAlignment(Pos.CENTER);
-    
+
     // keyframe handler
-    EventHandler<ActionEvent>handler = event->{
+    EventHandler<ActionEvent> handler = event -> {
       imgNum++;
       pane.getChildren().clear();
       imgLabel.setText("Image " + imgNum);
-      pane.getChildren().addAll(imgLabel, new ImageView(picName[imgNum-1]));
-      if (imgNum == n){
+      pane.getChildren().addAll(imgLabel, new ImageView(picName[imgNum - 1]));
+      if (imgNum == n) {
         imgNum = 0;
       }
     };
@@ -53,16 +55,16 @@ public class DigitalPictureFrame extends Application {
     // build timeline animation
     Timeline timeline = new Timeline(keyFrame);
     timeline.setCycleCount(Animation.INDEFINITE);
-    
-// add the image from the array to the pane
-  pane.getChildren().addAll(imgLabel, new ImageView(picName[imgNum-1]));
-  imgLabel.setText("Image " + imgNum);
-  
+
+    // add the image from the array to the pane
+    pane.getChildren().addAll(imgLabel, new ImageView(picName[imgNum - 1]));
+    imgLabel.setText("Image " + imgNum);
+
     // set the stage and show
     Scene scene = new Scene(pane);
     stage.setTitle("Digital Picture Frame");
     stage.setScene(scene);
     stage.show();
     timeline.playFromStart();
-  }  
+  }
 }
