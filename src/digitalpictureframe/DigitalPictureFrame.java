dbcc926238bcc1589387d9e9f5c70703eff2ebee
Gary A. Newsome <garyanewsome@gmail.com>
2016-03-12 15:16:51 -0500

    Assignment complete.
    Smells of completion.

--- src/digitalpictureframe/DigitalPictureFrame.java
+++ src/digitalpictureframe/DigitalPictureFrame.java
@@ -26,12 +26,11 @@ import javafx.util.Duration;
  */
 public class DigitalPictureFrame extends Application {
   String[] picName = {"blue.png", "green.png", "red.png"};
-  int n = picName.length;
-  int img = 1;
+  int n = picName.length; // magic number that makes the world go round
+  int imgNum = 1; // current image number
 
   @Override
   public void start(Stage stage) throws Exception {
-    ImageView iv = new ImageView();
     // create label
     Label imgLabel = new Label();
     // create a pane
@@ -40,20 +39,24 @@ public class DigitalPictureFrame extends Application {
     
     // keyframe handler
     EventHandler<ActionEvent>handler = event->{
-  //imgLabel.setText("Image " + img);
-  //pane.getChildren().addAll(imgLabel, new ImageView(picName[img]));
-      img++;
+      imgNum++;
+      pane.getChildren().clear();
+      imgLabel.setText("Image " + imgNum);
+      pane.getChildren().addAll(imgLabel, new ImageView(picName[imgNum-1]));
+      if (imgNum == n){
+        imgNum = 0;
+      }
     };
     // build the keyframe
-    Duration sec = new Duration(500);
+    Duration sec = new Duration(2000);
     KeyFrame keyFrame = new KeyFrame(sec, handler);
     // build timeline animation
     Timeline timeline = new Timeline(keyFrame);
     timeline.setCycleCount(Animation.INDEFINITE);
     
 // add the image from the array to the pane
-  pane.getChildren().addAll(imgLabel, new ImageView(picName[img-1]));
-  imgLabel.setText("Image " + img);
+  pane.getChildren().addAll(imgLabel, new ImageView(picName[imgNum-1]));
+  imgLabel.setText("Image " + imgNum);
   
     // set the stage and show
     Scene scene = new Scene(pane);
